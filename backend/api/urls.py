06249123ea6fb8f8c264c05d014cc89db3500b70
sekibaalamark <markmajje256@gmail.com>
2025-06-20 19:12:25 +0300

    trial

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register-user'),
-    path('create-medical-bill/', MedicalRecordCreateAPIView.as_view(), name='create-medical-record'),
     path('medical-bills-sum/',CDOBillingSummaryAPIView.as_view(),name='bill_summary'),
     path('login/', login, name='login'),
     path('create-child/',CreateChildAPIView.as_view(),name='create-child'),
